74d3f9480a496f6598f49781ebd0899bf71fc5ef
bebe-dax <yubebe.2010@gmail.com>
2026-01-01 16:10:33 +0900

    🎉Initialize project

---
+++ components/features/Fretborad/Fretboard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Guitar from "react-guitar";
+
+const Fretboard = () => {
+  return (
+    <div>
+      <Guitar strings={[0, 1, 2, 2, 0, -1]} />
+    </div>
+  );
+};
+
+export default Fretboard;
