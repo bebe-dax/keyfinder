387f8b05304c1105ade26b048192e8f047e37cd4
bebe-dax <yubebe.2010@gmail.com>
2026-04-27 22:20:51 +0900

    ✨ Add Pitch names

--- components/features/Fretborad/Fretboard.tsx
+++ components/features/Fretborad/Fretboard.tsx
@@ -1,8 +1,18 @@
 "use client";
 
-import React from "react";
 import Guitar from "react-guitar";
+import { Note } from "tonal";
 import { getChordPosition } from "./chordPositions";
+
+// Standard tuning: E2 A2 D3 G3 B3 E4 (MIDI numbers, low to high)
+const STANDARD_TUNING = [40, 45, 50, 55, 59, 64];
+
+function renderFinger(stringIndex: number, fret: number) {
+  const midi = STANDARD_TUNING[stringIndex] + fret;
+  const note = Note.fromMidi(midi);
+  const { letter, acc } = Note.get(note);
+  return <span>{letter}{acc}</span>;
+}
 
 interface FretboardProps {
   chord?: string;
@@ -12,7 +22,7 @@ export const Fretboard = ({ chord }: FretboardProps) => {
   const strings = chord ? getChordPosition(chord) : [-1, -1, -1, -1, -1, -1];
   return (
     <div>
-      <Guitar strings={strings} />
+      <Guitar strings={strings} renderFinger={renderFinger} />
     </div>
   );
 };
